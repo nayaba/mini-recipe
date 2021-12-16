1fed3113946bf8e6277fefc9f87d684de7f508d3
nayaba <nabila.ayaba@gmail.com>
2021-12-15 16:43:17 -0800

    Begin implementing oauth with passport

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session')
+const passport = require('passport')
+const methodOverride = require('method-override');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 require('dotenv').config()
 require('./config/database')
+require('./config/passport')
 
 var app = express();
 
@@ -20,6 +24,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
