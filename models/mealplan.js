efd69a31b0ac3e616e69d4c13e3758716803a17b
nayaba <nabila.ayaba@gmail.com>
2021-12-20 15:58:18 -0800

    Create Mealplan Schema

---
+++ models/mealplan.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const mealplanSchema = new Schema({
+    date: {
+        type: String,
+        required: true
+    },
+    meal: {
+        type: String,
+        enum: ['Breakfast', 'Lunch', 'Dinner', 'Dessert', 'Snack']
+    },
+    recipe: [{type: Schema.Types.ObjectId, ref: 'Recipe'}]
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Mealplan', mealplanSchema)
