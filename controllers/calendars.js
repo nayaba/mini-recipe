cf65edc2e6a5e6e50be2fb7941bf5e789e2fd334
nayaba <nabila.ayaba@gmail.com>
2021-12-22 14:04:59 -0800

    Populate recipes in meal plan form

--- controllers/calendars.js
+++ controllers/calendars.js
@@ -1,6 +1,7 @@
 // import our calcTable function from config file
 const calendar = require('../config/calendar')
 const MealPlan = require('../models/mealplan')
+const Recipe = require('../models/recipe')
 // const scriptFile = require('../public/javascripts/calendar')
 
 module.exports = {
@@ -23,16 +24,19 @@ function index(req, res) {
 }
 
 // show function
-function show(req, res) {
+async function show(req, res) {
     const year = req.query.year || 2022
     // will update to take info from req
     const month = 3
     const showMonth = months[month]
 
+    let recipes = await Recipe.find({})
+    
     res.render('calendars/show', {
         calendar: calendar(year),  
         year,
         month,
+        recipes,
         title: `${showMonth} ${year}`
     })
 }
